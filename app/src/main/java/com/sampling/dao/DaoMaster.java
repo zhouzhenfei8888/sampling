05ff6758606d28726f79b27575ccbf30f8d55d84
zzf <8zzf@sina.com>
2018-03-23 10:03:45 +0800

    0

--- app/src/main/java/com/sampling/dao/DaoMaster.java
+++ app/src/main/java/com/sampling/dao/DaoMaster.java
@@ -22,12 +22,14 @@ public class DaoMaster extends AbstractDaoMaster {
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
         DictionaryDao.createTable(db, ifNotExists);
+        FoodDictBeanDao.createTable(db, ifNotExists);
         SamplingBeanDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
         DictionaryDao.dropTable(db, ifExists);
+        FoodDictBeanDao.dropTable(db, ifExists);
         SamplingBeanDao.dropTable(db, ifExists);
     }
 
@@ -48,6 +50,7 @@ public class DaoMaster extends AbstractDaoMaster {
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
         registerDaoClass(DictionaryDao.class);
+        registerDaoClass(FoodDictBeanDao.class);
         registerDaoClass(SamplingBeanDao.class);
     }
 
